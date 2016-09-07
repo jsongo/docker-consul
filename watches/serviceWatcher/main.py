f448b1c90d46316cd11154b761fc3b29d50f3bc6
jsongo <jsongo@qq.com>
2016-09-07 11:54:36 +0800

    add supervisor

--- watches/serviceWatcher/main.py
+++ watches/serviceWatcher/main.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # coding=utf-8
-import sys
+import urllib2
+import json as JSON
 
 if __name__ == '__main__':
+    rsp = urllib2.urlopen('http://consul.jinyuen.com/v1/health/state/critical')
+    content = rsp.read()
+    result = JSON.loads(content)
+    for data in result:
+        service = data.get('ServiceID')
+        if service[-6:] == 'server': # 是个service
+            pass # TODO: restart
+
     path = '/data/test.txt'
     with open(path, 'wb') as f:
-        f.write(str(sys.argv))
+        f.write(content)
