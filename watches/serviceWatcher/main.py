4e08c91099f27ae6dd1f68178d596745433cb537
jsongo <jsongo@qq.com>
2016-09-07 11:25:39 +0800

    update service watcher

--- watches/serviceWatcher/main.py
+++ watches/serviceWatcher/main.py
@@ -3,6 +3,6 @@
 import sys
 
 if __name__ == '__main__':
-    path = '/data/code/dockerfile/consul/watches/serviceWatcher/test.txt'
+    path = '/data/test.txt'
     with open(path, 'wb') as f:
         f.write(str(sys.argv))
